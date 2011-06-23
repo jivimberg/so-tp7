e822be2b7d07b243c17cdd676e02ed9d461fd727
jivimberg <jivimberg@38cfc638-54d3-6755-dc68-c6e1278ac511>
2011-06-23 08:51:43 +0000

    Some improvements

--- tp7/src/main/java/org/austral/so/tps/tp7/algorithms/FIFOW2CAlgorithm.java
+++ tp7/src/main/java/org/austral/so/tps/tp7/algorithms/FIFOW2CAlgorithm.java
@@ -1,15 +1,34 @@
 package org.austral.so.tps.tp7.algorithms;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.austral.so.tps.tp7.model.core.Page;
+import org.austral.so.tps.tp7.model.core.PageFrame;
 import org.austral.so.tps.tp7.model.core.ProcessPageTable;
 import org.austral.so.tps.tp7.model.memories.OSRAM;
 
-public class FIFOW2CAlgorithm implements PageReplacingAlgorithm {
+public class FIFOW2CAlgorithm implements PageReplacingAlgorithm{
 
-	public int computePageToReplace(OSRAM ram, int pageNumber, ProcessPageTable processPageTable) {
-		return ram.getLastWrittenLocation();
+	Queue<PageFrame> queue;
+	
+	public FIFOW2CAlgorithm(){
+		queue = new LinkedList<PageFrame>(); 
+	}
+	
+	public String getName() {
+		return "FIFOW2C";
 	}
 
-	public String getName() {
-		return "FIFO";
+	@Override
+	public int computePageToReplace(OSRAM ram, ProcessPageTable processPageTable) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void pageReferenced(Page page) {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ tp7/src/main/java/org/austral/so/tps/tp7/listeners/OSListener.java
@@ -0,0 +1,9 @@
+package org.austral.so.tps.tp7.listeners;
+
+import org.austral.so.tps.tp7.model.core.Page;
+
+public interface OSListener {
+	
+	void pageReferenced(Page page);
+
+}
